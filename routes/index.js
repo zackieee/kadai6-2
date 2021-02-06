d62f05618d0206e894613a56e437947316037cc8
zackieee <zackie.jackie.222@gmail.com>
2021-02-06 15:17:32 +0900

    Cookie確認用のコードを追加

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.setHeader('set-cookie', [
+    'name=hoge',
+  ]);
   res.render('index', {
     title: 'Express',
     host: req.headers.host,
